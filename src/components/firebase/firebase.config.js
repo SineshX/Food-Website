e7138203ee7bfdbe9ced588a274d9c3cc014887d
SineshX <Sineshkumarr@gmail.com>
2021-12-15 13:14:20 +0530

    first commit

---
+++ src/components/firebase/firebase.config.js
@@ -0,0 +1,20 @@
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDnfFKiBycU68dQaglYn6vN6_vPxRUBSbY",
+  authDomain: "romo-07.firebaseapp.com",
+  projectId: "romo-07",
+  storageBucket: "romo-07.appspot.com",
+  messagingSenderId: "841991266531",
+  appId: "1:841991266531:web:6d4ab16bd73faa451be5d6",
+  measurementId: "G-90VVLDXMXF"
+};
+
+// Initialize Firebase
+
+export default firebaseConfig;
+
